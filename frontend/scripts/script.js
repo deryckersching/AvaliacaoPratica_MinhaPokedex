d7d94cd3d2d9b5437ab944186d79ad19e53d7211
deryckersching <deryckersching@gmail.com>
2026-09-14 21:33:22 -0300

    Dei uma acrescentada e mudada no html e javaScript no dia 14/09/26

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -3,7 +3,7 @@ let todosPokemons = [];
 
 async function pegarPokemons() {
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
         const data = await response.json();
         console.log(data)
 
@@ -25,16 +25,57 @@ async function pegarPokemons() {
 
 function renderizarPokemons() {
     console.log(todosPokemons);
-    todosPokemons.forEach(pokemon => {
-        pokemonsGrid.innerHTML += `
-            <div class="card col-3" style="width: 18rem;">
-                <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="${pokemon.name}">
-                <div class="card-body">
-                    <h5 class="card-title">${pokemon.name}</h5>
-                </div>
-            </div>
-        `;
+
+todosPokemons.forEach(pokemon => {
+    const coluna = document.createElement("div");
+    coluna.classList.add("col-3");
+
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const imagem = document.createElement("img");
+    imagem.src = pokemon.sprites.other["official-artwork"].front_default;
+    imagem.alt = pokemon.name;
+    imagem.classList.add("card-img-top");
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const nome = document.createElement("h5");
+    nome.textContent = pokemon.name;
+    nome.classList.add("card-title");
+
+    card.appendChild(imagem);
+    cardBody.appendChild(nome);
+    card.appendChild(cardBody);
+    coluna.appendChild(card);
+    pokemonsGrid.appendChild(coluna);
+
+    card.addEventListener("click", () => {
+        abrirModal(pokemon);
+
+        });
+
     });
-};
+
+}
+
+
+
+function abrirModal(pokemon) {
+    const modalTitulo = document.getElementById("modalTitulo");
+    const modalConteudo = document.getElementById("modalConteudo");
+    const modal = document.getElementById("pokemonModal");
+    modalTitulo.textContent = pokemon.name;
+    
+    modalConteudo.innerHTML = `
+        <img
+            src="${pokemon.sprites.other["official-artwork"].front_default}"
+            alt="${pokemon.name}"
+        >
+    `;
+
+}
 
 pegarPokemons();
